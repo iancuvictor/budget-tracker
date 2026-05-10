acc7dee46005f2e73a0283440b7cf97fa7fcbd77
Iancu Victor-Iuliu <iancuvictorwork@gmail.com>
2026-05-10 15:32:49 +0300

    discovered that i can write html straight into javascript. pretty dope, went ahead of schedule with the code writing.

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,37 @@
+let incomeList = document.querySelector('#incomeList');
+let expenseList = document.querySelector('#expenseList');
+
+// Functions
 
 //Type = expense, income
-function createInput(type, ){
-    let div = document.createElement('div');
-    div.className= `${type}Box`;
-    div.innerHTML = `${type} body goes here`;
+function createInput(type, location){
+    // let div = document.createElement('div');
+    // div.className= `${type}Box`;
+    // div.innerHTML = `${type} body goes here`;
 
-    document.body.append(div);
+    location.insertAdjacentHTML('beforeend', `
+    <div class='${type}Box'>
+    <span>${type} goes here</span>
+    </div>`);
 }
 
+// function sum(array){
+//     let sum = 0;
+//     for(i = 0; i < array.length; i++){
+//         sum = sum + array[i];
+//     };
+//     return sum;
+// }
+
+// Event listeners
+
+// Event listener for ADDING EXPENSES
 document.querySelector('#createExpenseBtn').addEventListener('click', () => {
-    createInput('Expense')
+    createInput('Expense', expenseList)
 });
 
+// Event listener for ADDING INCOMES
 document.querySelector('#createIncomeBtn').addEventListener('click', () => {
-    createInput('Income');
+    createInput('Income', incomeList);
 });
 
