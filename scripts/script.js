ebf8915bf607d53a18d07f03a13d68173c337c5f
Iancu Victor-Iuliu <iancuvictorwork@gmail.com>
2026-05-11 09:28:07 +0300

    Added: event listener for removing rows (be it income or expense); income calculator event (will add expense now); added function initialiseTable so nothing remains hardcoded in the html (table wise); added function generateArray to get array for each sum, and sum function to sum them. i could combine them but for now they remain as is;

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,78 @@
-let incomeList = document.querySelector('#incomeList');
-let expenseList = document.querySelector('#expenseList');
+let incomeList = document.querySelector("#incomeList");
+let expenseList = document.querySelector("#expenseList");
 
 // Functions
-
 //Type = expense, income
-function createInput(type, location){
-    // let div = document.createElement('div');
-    // div.className= `${type}Box`;
-    // div.innerHTML = `${type} body goes here`;
-
-    location.insertAdjacentHTML('beforeend', `
-    <div class='${type}Box'>
-    <span>${type} goes here</span>
-    </div>`);
+function createInput(type = "Enter name", location) {
+  let object = location.insertAdjacentHTML(
+    "beforeend",
+    `
+    <li>
+        <input type="text" placeholder="${type} name">
+        <input type="number" class="${type}Input" placeholder="0,00" />
+        <button class="selfDestroyBTN">delete</button>
+    </li>
+    `,
+  );
 }
 
-// function sum(array){
-//     let sum = 0;
-//     for(i = 0; i < array.length; i++){
-//         sum = sum + array[i];
-//     };
-//     return sum;
-// }
+// Initialise the table for each type
+function initialiseTable(length, type, location) {
+  for (let i = 0; i < length; i++) {
+    createInput(type, location);
+  }
+}
 
-// Event listeners
+initialiseTable(2, "Income", incomeList);
+initialiseTable(2, "Expense", expenseList);
 
+function generateArray(type) {
+  let array = [];
+  let inputs = document.querySelectorAll(`.${type}Input`);
+  for (let i = 0; i < inputs.length; i++) {
+    array.push(inputs[i].value);
+  }
+  return array;
+  alert(array);
+}
+//let expenseArray = generateArray("expense");
+
+function sum(type) {
+  let incomeArray = generateArray("income");
+  let total = document.querySelector(`#total${type}`);
+  //let inputs = document.querySelectorAll(`${type}Input`)
+  let sum = 0;
+  for (i = 0; i < incomeArray.length; i++) {
+      sum = +sum + +incomeArray[i];
+  }
+
+  total.value = sum;
+  alert(sum);
+}
+
+// sum(incomeArray, "Income");
+// sum(expenseArray, "Expense");
+
+
+
+// EVENT LISTENER
 // Event listener for ADDING EXPENSES
-document.querySelector('#createExpenseBtn').addEventListener('click', () => {
-    createInput('Expense', expenseList)
+document.querySelector("#createExpenseBtn").addEventListener("click", () => {
+  createInput("Expense", expenseList);
 });
 
 // Event listener for ADDING INCOMES
-document.querySelector('#createIncomeBtn').addEventListener('click', () => {
-    createInput('Income', incomeList);
+document.querySelector("#createIncomeBtn").addEventListener("click", () => {
+  createInput("Income", incomeList);
 });
 
+// Event listener for REMOVING anything.
+document.addEventListener("click", () => {
+  if (event.target.classList.contains("selfDestroyBTN")) {
+    event.target.parentElement.remove();
+  }
+});
+
+document.querySelector("#calculateIncomeBTN").addEventListener("click", () => {
+  sum("Income");
+});
